e437f66300598b5f2d3a1d8a3b0172f227ff906c
s28403 <s28403@pjwstk.edu.pl>
2024-03-19 11:38:32 +0100

    Made class Containar and some methods

--- Project02/Program.cs
+++ Project02/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Dynamic;
+
+public interface IHazardNotifier{
+    void NotifyHazard(string containerNumber, string messege);
+}
+
+public class Container(string serialNumber, double height, double tareWeight, double depth, double maxPayload)
+{
+    public string SerialNumber { get; } = serialNumber;
+    public double CargoMass{get; set;}
+    public double Height { get; } = height;
+    public double TareWeight { get; } = tareWeight;
+    public double Depth { get; } = depth;
+    public double MaxPayload { get; } = maxPayload;
+
+
+public virtual void LoadCargo(double cargoMass){
+    if (cargoMass > MaxPayload)
+    {
+        throw new OverfillException("Cargo is overfilled.");
+    }
+    CargoMass = cargoMass;
+}
+
+public virtual void EmptyCargo()
+{
+    CargoMass = 0;
+}
+}
+public class OverfillException(string message) : Exception(message)
+{
+}
